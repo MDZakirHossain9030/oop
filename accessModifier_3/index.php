205e342ba53c0d4114b8babf578731165e88080f
MD ZAKIR HOSSAIN <159409026+MDZakirHossain9030@users.noreply.github.com>
2025-12-01 21:16:46 -0800

    Update index.php

--- accessModifier_3/index.php
+++ accessModifier_3/index.php
@@ -6,7 +6,7 @@
 
 //public property (পাবলিক টয়লেটের মত) বাহির থেকে access করা যায় |
 //private property (ব্যক্তিগত ট্যলেটের মত) বাহির থেকে access করা যায় না |
-//protected property বাহির থেকে access করা যায় না কিন্তু এটার মাধ্যমে perent class এর কাজ child class এ আক্সেস করা যায়, কিন্তু perent class এর কাজ child class এ আক্সেস করতে না চাইলে perent class এর সামনে final keyword দিলে আর child class এ এক্সেস করা যাবে না |
+//protected property (সুরক্ষিত) বাহির থেকে access করা যায় না কিন্তু এটার মাধ্যমে perent class এর কাজ child class এ আক্সেস করা যায়, কিন্তু perent class এর কাজ child class এ আক্সেস করতে না চাইলে perent class এর সামনে final keyword দিলে আর child class এ এক্সেস করা যাবে না |
 
 
 
